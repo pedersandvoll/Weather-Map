517153ab2a17f3c0923a1f6f06d750b80567230d
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2023-01-24 12:54:41 +0100

    Update index.js

--- Weather_Map/index.js
+++ Weather_Map/index.js
@@ -1,6 +1,29 @@
+var link = document.createElement("link");
+link.href = "style.css";
+
 // map
 
-var map = L.map("map").setView([63.43, 10.4], 5);
+L.mapquest.key = "ck2OXUAJsF0iz999XGQ62jyXo8AXOVp7";
+
+var map = L.mapquest.map("map", {
+  center: [60.472, 8.4689],
+  layers: L.mapquest.tileLayer("map"),
+  zoom: 5,
+});
+
+var directions = L.mapquest
+  .directionsControl({
+    routeSummary: {
+      enabled: true,
+    },
+    narrativeControl: {
+      enabled: true,
+      compactResults: true,
+    },
+  })
+  .addTo(map);
+
+// tilelayer
 
 var basicDark = L.tileLayer(
   "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
@@ -9,12 +32,9 @@ var basicDark = L.tileLayer(
   }
 ).addTo(map);
 
-var basicLight = L.tileLayer(
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png",
-  {
-    minZoom: 3,
-  }
-);
+var basicLight = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  minZoom: 3,
+});
 
 var advanced = L.tileLayer(
   "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
@@ -31,11 +51,54 @@ var baseMaps = {
   Advanced: advanced,
 };
 
-L.control.layers(baseMaps).addTo(map);
-
 map.addControl(new L.Control.Fullscreen());
 
 L.control.locate().addTo(map);
+
+// search
+
+var search = placeSearch({
+  key: "ucOKfYhC4s2tu4lumOn4CmZtcEy24PQH",
+  container: document.querySelector("#place-search-input"),
+});
+
+var searchMarker = null;
+
+search.on("change", (e) => {
+  console.log(e);
+  if (searchMarker !== null) {
+    map.removeLayer(searchMarker);
+  }
+  const response = fetch(
+    `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${e.result.latlng.lat}&lon=${e.result.latlng.lng}`
+  )
+    .then((r) => r.json())
+    .then((data) => {
+      var weather = data.properties.timeseries[0];
+      console.log(data.properties.timeseries[0]);
+
+      var weatherIcon = L.icon({
+        iconUrl: `Assets/weather_icons/${weather.data.next_1_hours.summary.symbol_code}.png`,
+        iconSize: [50, 50],
+        iconAnchor: [0, 55],
+        popupAnchor: [30, -45],
+      });
+
+      searchMarker = L.marker([e.result.latlng.lat, e.result.latlng.lng], {
+        icon: weatherIcon,
+      });
+      searchMarker.bindPopup(
+        `Det er ${Math.round(
+          weather.data.instant.details.air_temperature
+        )}°C i ${e.result.name}!`
+      );
+      searchMarker.addTo(map);
+      searchMarker.openPopup();
+      searchMarker.getPopup().on("remove", function () {
+        map.removeLayer(searchMarker);
+      });
+    });
+});
 
 // show markers and popup when markers is clicked
 
@@ -93,46 +156,53 @@ locationLayer.addTo(map);
 
 // view degree based on coordinates
 
-var newMarker = {};
-map.on("click", function (e) {
-  fetch(
-    "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=" +
-      e.latlng.lat +
-      "&lon=" +
-      e.latlng.lng
-  )
-    .then((r) => r.json())
-    .then((data) => {
-      lat = e.latlng.lat;
-      lon = e.latlng.lng;
-      console.log("You clicked the map at LAT: " + lat + " and LONG: " + lon);
-      if (newMarker != undefined) {
-        map.removeLayer(newMarker);
-      }
-      var weather = data.properties.timeseries[0];
+// var newMarker = {};
+// map.on("click", function (e) {
+//   fetch(
+//     "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=" +
+//       e.latlng.lat +
+//       "&lon=" +
+//       e.latlng.lng
+//   )
+//     .then((r) => r.json())
+//     .then((data) => {
+//       lat = e.latlng.lat;
+//       lon = e.latlng.lng;
+//       console.log("You clicked the map at LAT: " + lat + " and LONG: " + lon);
+//       if (newMarker != undefined) {
+//         map.removeLayer(newMarker);
+//       }
+//       var weather = data.properties.timeseries[0];
 
-      var weatherIcon = L.icon({
-        iconUrl: `Assets/weather_icons/${weather.data.next_1_hours.summary.symbol_code}.png`,
+//       var weatherIcon = L.icon({
+//         iconUrl: `Assets/weather_icons/${weather.data.next_1_hours.summary.symbol_code}.png`,
 
-        iconSize: [50, 50],
-        iconAnchor: [20, 20],
-        popupAnchor: [0, -10],
-      });
+//         iconSize: [50, 50],
+//         iconAnchor: [20, 20],
+//         popupAnchor: [0, -10],
+//       });
 
-      newMarker = L.marker([lat, lon], { icon: weatherIcon }).addTo(map);
-      newMarker
-        .bindPopup(
-          `Det er ${Math.round(
-            weather.data.instant.details.air_temperature
-          )}°C!`
-        )
-        .openPopup();
-    });
-});
-if ((newMarker = onclick)) {
-  map.removeLayer(newMarker);
-  closure(marker);
-}
+//       newMarker = L.marker([lat, lon], { icon: weatherIcon }).addTo(map);
+//       newMarker
+//         .bindPopup(
+//           `Det er ${Math.round(
+//             weather.data.instant.details.air_temperature
+//           )}°C!`
+//         )
+//         .openPopup();
+//         newMarker.getPopup().on('remove', function() {
+//           map.removeLayer(newMarker);
+//         });
+//     });
+// });
+// if ((newMarker = onclick)) {
+//   map.removeLayer(newMarker);
+//   closure(marker);
+// }
+
+// leaflet layer control
+
+L.control.layers(baseMaps).addTo(map);
 
 // hide/show markers
 
@@ -142,7 +212,7 @@ command.onAdd = function (map) {
   var div = L.DomUtil.create("div", "command");
 
   div.innerHTML =
-    '<input checked id="command" type="checkbox" name="chk"/><label for="chk"></label>';
+    '<input checked id="command" type="checkbox" name="chk"/><label class="chk"></label>';
   return div;
 };
 
